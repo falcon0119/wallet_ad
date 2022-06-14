6bf8c528f30e3bad457e02c54aae597316c62915
sockeye119 <58209598+sockeye119@users.noreply.github.com>
2022-06-14 14:42:27 +0900

    “コミット名”

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -71,7 +71,7 @@ export default function SearchAppBar() {
             component="div"
             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            MUI
+            取引検索サイト
           </Typography>
           <Search>
             <SearchIconWrapper>
